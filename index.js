722eaa1a4428738fe6c93f90745e5488910e09f4
jyotiprakash-m <teachmeonline365@gmail.com>
2021-05-27 21:05:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function togglePopup() {
+    document.getElementById("popup-1").classList.toggle("active");
+}
+function togglePopup2() {
+    document.getElementById("popup-2").classList.toggle("active");
+}
+function headingToggle1() {
+    document.getElementById("heading1").classList.add("active-heading");
+    document.getElementById("heading2").classList.remove("active-heading");
+}
+function headingToggle2() {
+    document.getElementById("heading2").classList.add("active-heading");
+    document.getElementById("heading1").classList.remove("active-heading");
+}
+function headingToggle3() {
+    document.getElementById("heading3").classList.add("active-heading");
+    document.getElementById("heading4").classList.remove("active-heading");
+}
+function headingToggle4() {
+    document.getElementById("heading4").classList.add("active-heading");
+    document.getElementById("heading3").classList.remove("active-heading");
+}
